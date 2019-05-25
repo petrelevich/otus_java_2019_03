e698b9b4055e8b4522e5b20f1fc46ac12b50e670
sergey <petrelevich@yandex.ru>
2019-05-25 11:49:05 +0300

    L13 Structural Patterns

---
+++ L13-structuralPatterns/src/main/java/ru/otus/composite/Demo.java
@@ -0,0 +1,29 @@
+package ru.otus.composite;
+
+/**
+ * @author sergey
+ * created on 16.01.19.
+ */
+public class Demo {
+    public static void main(String[] args) {
+        Unit marine1 = new Marine();
+        Unit marine2 = new Marine();
+        Unit marine3 = new Marine();
+
+        Unit tank = new Tank();
+
+        Group group = new Group();
+        group.addUnit(marine1);
+        group.addUnit(marine2);
+        group.addUnit(marine3);
+        group.addUnit(tank);
+
+        group.move();
+
+        Group group2 = new Group();
+        group2.addUnit(group);
+        group2.addUnit(new Tank());
+
+        group2.move();
+    }
+}
