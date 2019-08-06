1ceb9df62026e26d3301b57fe41331220feabe95
sergey <petrelevich@yandex.ru>
2019-08-07 00:58:21 +0300

    L30 Executors

---
+++ L30-executors/src/main/java/ru/otus/executors/CompletableFutureDemo.java
@@ -0,0 +1,55 @@
+package ru.otus.executors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+public class CompletableFutureDemo {
+    private static Logger logger = LoggerFactory.getLogger(CompletableFutureDemo.class);
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        new CompletableFutureDemo().go();
+    }
+
+    private void go() throws ExecutionException, InterruptedException {
+     //   CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> task(1));
+     //   logger.info("thread is not blocked");
+     //   logger.info("result:{}", future1.get());
+
+    //    CompletableFuture<String> future2 = CompletableFuture.supplyAsync(() -> task(2));
+    //    future2.thenAccept(val -> logger.info("result:{}", val));
+    //    future2.join();
+
+    //    CompletableFuture<String> future3 = CompletableFuture.supplyAsync(this::errorAction);
+    //    future3.exceptionally(Throwable::getMessage).thenAccept(msg -> logger.info("msg:{}", msg));
+    }
+
+    private String errorAction() {
+        throw new TestException("error for Test");
+    }
+
+
+    private String task(int id) {
+        sleep();
+        logger.info("task is done: {}", id);
+        return "done" + id;
+    }
+
+    private static void sleep() {
+        try {
+            Thread.sleep(TimeUnit.SECONDS.toMillis(3));
+        } catch (InterruptedException e) {
+            logger.error(e.getMessage());
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private class TestException extends RuntimeException {
+        TestException(String msg) {
+            super(msg);
+        }
+    }
+}
